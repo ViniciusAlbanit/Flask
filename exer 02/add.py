f92d13e062422b1897f101835cc86779b5d893ac
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-10-05 10:25:00 -0300

    new

--- exer 02/add.py
+++ exer 02/add.py
@@ -6,5 +6,8 @@ app = Flask(__name__)
 def index():
     resposta = render_template('index.html')
     return resposta
-
+@app.route('/exemplo1')
+def exemplo1():
+    pessoa = {"nome":"Vinicius", "matricula":2200420}
+    return render_template('exemplo1.html', dados=pessoa)
 app.run(debug=True)
