b9fac5cb709494996c693474e2a0332b1c3d2870
Vinicius Farias <102328663+ViniciusAlbanit@users.noreply.github.com>
2022-10-19 23:42:07 -0300

    new

---
+++ envio_de_formularios/projeto_exemplos_formularios/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    nome = request.form.get("nome")
+    idade = request.form.get("idade")
+    if nome == None or idade == None:
+        return render_template('formulario.html')
+    else:
+        return f"O nome é: {nome}, e a idade é: {idade}"
+
+app.run(debug=True)
